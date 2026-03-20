0fd9a83a6091f7cf1cbb5bc6f05352c3fffad921
Neeraja824 <lankaneeraja824@gmail.com>
2026-03-20 21:04:58 +0530

    files

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*------------------------toggle icon navbar------------------*/
 let menuIcon=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
 
@@ -6,7 +5,7 @@ menuIcon.onclick=()=>{
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 }
-/*------------------------scroll section active link------------------*/
+
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header .navbar a');
 window.onscroll=()=>{
@@ -22,35 +21,31 @@ window.onscroll=()=>{
             });
         };
     });
-/*------------------------sticky navbar------------------*/
 let header=document.querySelector('header');
 header.classList.toggle('sticky',window.scrollY>100);
-/*------------------------remove toggle icon and navbar when click navbar link (scroll)------------------*/
 menuIcon.classList.remove('fa-xmark');
 navbar.classList.remove('active');
 };
-/*------------------------scroll reveal------------------*/
-ScrollReveal({ 
+
+ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200,
 });
-ScrollReveal().reveal('.certifications .heading1', { 
-    origin: 'top', 
-    distance: '50px', 
-    duration: 2000, 
-    delay: 200 
+ScrollReveal().reveal('.certifications .heading1', {
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    delay: 200
 });
-
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-/*------------------------scroll reveal for certifications------------------*/
 ScrollReveal().reveal('.certifications-container, .certification-box', { origin: 'bottom' });
 ScrollReveal().reveal('.certification-layer h4, .certification-layer p, .certification-layer a', { origin: 'top', interval: 200 });
-/*------------------------typed js------------------*/
+
 const typed=new Typed('.multiple-text',{
     strings: ['Full Stack Developer','Web Designer'],
     typeSpeed:100,
@@ -58,6 +53,7 @@ const typed=new Typed('.multiple-text',{
     backDelay:1000,
     loop:true,
 });
+
 function showLayer(button) {
     const layer = button.closest('.certification-box').querySelector('.certification-layer');
     layer.classList.add('show');
